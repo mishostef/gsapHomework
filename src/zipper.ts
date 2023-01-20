335b93d2fb68cf2f314a601920fc72511e2506fe
mihail.stefanov <mihail.stefanov@amusnet.com>
2023-01-20 23:51:09 +0200

    added logic on mouseMove

--- src/zipper.ts
+++ src/zipper.ts
@@ -4,6 +4,7 @@ import { gsap } from "gsap";
 import { PixiPlugin } from "gsap/PixiPlugin";
 import { createInteractiveBg } from "./utils";
 const verOffset = 30;
+let mouseLastY = CANVAS_HEIGHT;
 
 gsap.registerPlugin(PixiPlugin);
 PixiPlugin.registerPIXI(PIXI);
@@ -18,15 +19,59 @@ const bg = createInteractiveBg();
 app.stage.addChild(bg);
 
 const [left, right] = getTrapezoids();
-
+let indicesLeft = [];
+let indicesRight = [];
 bg.on("mousemove", ({ globalX: x, globalY: y }) => {
   console.log(`current x is ${x},     current y is ${y}`);
-  left.forEach((zip) => {
-    if (y >= zip.y - 50 && y <= zip.y + 50) {
-      zip.x -= 100;
-    }
-  });
+  let direction = "down";
+  if (y < mouseLastY) {
+    direction = "up";
+  }
+  mouseLastY = y;
+  if (direction === "down") {
+    left.forEach((zip, i) => {
+      if (isLeftOpened(y, zip, i)) {
+        zip.x -= 20;
+        indicesLeft.push(i);
+      }
+    });
+    right.forEach((zip, i) => {
+      if (isRightOpened(y, zip, i)) {
+        zip.x += 20;
+        indicesRight.push(i);
+      }
+    });
+  } else {
+    left.forEach((zip, i) => {
+      if (isLeftClosed(y, zip, i)) {
+        zip.x += 20;
+        indicesLeft = indicesLeft.filter((index) => index !== i);
+      }
+    });
+    right.forEach((zip, i) => {
+      if (isRightClosed(y, zip, i)) {
+        zip.x -= 20;
+        indicesRight = indicesRight.filter((index) => index !== i);
+      }
+    });
+  }
 });
+
+function isRightClosed(y: number, zip: PIXI.Graphics, i: number) {
+  return y >= zip.y - 20 && y <= zip.y + 20 && indicesRight.includes(i);
+}
+
+function isLeftClosed(y: number, zip: PIXI.Graphics, i: number) {
+  return y >= zip.y - 20 && y <= zip.y + 20 && indicesLeft.includes(i);
+}
+
+function isLeftOpened(y: number, zip: PIXI.Graphics, i: number) {
+  return y >= zip.y - 20 && y <= zip.y + 20 && !indicesLeft.includes(i);
+}
+
+function isRightOpened(y: number, zip: PIXI.Graphics, i: number) {
+  return y >= zip.y - 20 && y <= zip.y + 20 && !indicesRight.includes(i);
+}
 
 function getTrapezoids() {
   const left: PIXI.Graphics[] = [];
