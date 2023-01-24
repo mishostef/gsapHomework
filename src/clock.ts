4b8cc8bc5625983c8ab42bc66a73623efc8ecce4
mihail.stefanov <mihail.stefanov@amusnet.com>
2023-01-24 20:59:09 +0200

    added timeline in clock.ts

--- src/clock.ts
+++ src/clock.ts
@@ -20,78 +20,99 @@ const turnHours = hours > 12 ? hours - 3 : hours - 15;
 //gsapSetGsapToVariant();
 setIntervalVariant();
 //initClock();
+
 function initClock() {
   secHand.rotation = (turnSec * Math.PI) / 30;
   minHand.rotation = (turnMin * Math.PI) / 30;
   hourHand.rotation = (turnHours * Math.PI) / 6;
-  gsap.to(secHand, {
+  const tl = gsap.timeline();
+  tl.to(secHand, {
     pixi: { rotation: "+=360" },
     duration: 60,
     repeat: -1,
     ease: "linear",
   });
-  gsap.to(minHand, {
+  tl.to(minHand, {
     pixi: { rotation: "+=360" },
     duration: 60 * 60,
     repeat: -1,
     ease: "linear",
   });
-  gsap.to(hourHand, {
+  tl.to(hourHand, {
     pixi: { rotation: "+=360" },
     duration: 60 * 60 * 12,
     repeat: -1,
     ease: "linear",
   });
+  tl.pause(0);
+  tl.seek(0.1);
+  tl.play();
 }
+
 function gsapSetGsapToVariant() {
+  const tl = gsap.timeline({ paused: true });
   const [hours, minutes, sec] = getSecondsMinutesHours();
   const turnSec = sec - 15;
   const turnMin = minutes - 15;
   const turnHours = hours < 12 ? hours - 3 : hours - 15;
-  gsap.set(secHand, {
+
+  tl.set(secHand, {
     pixi: { rotation: turnSec * 6 },
   });
-  gsap.set(minHand, {
+  tl.set(minHand, {
     pixi: { rotation: turnMin * 6 },
   });
-  gsap.set(hourHand, {
+  tl.set(hourHand, {
     pixi: { rotation: turnHours * 30 },
   });
-  gsap.to(secHand, {
+
+  tl.to(secHand, {
     pixi: { rotation: "+=360" },
     duration: 60,
     repeat: -1,
     ease: "linear",
   });
-  gsap.to(minHand, {
+  tl.to(minHand, {
     pixi: { rotation: "+=360" },
     duration: 60 * 60,
     repeat: -1,
     ease: "linear",
   });
-  gsap.to(hourHand, {
+  tl.to(hourHand, {
     pixi: { rotation: "+=360" },
     duration: 60 * 60 * 12,
     repeat: -1,
     ease: "linear",
   });
+
+  setTimeout(() => {
+    tl.play();
+  }, 0.2);
 }
 
 function setIntervalVariant() {
+  const tl = gsap.timeline();
+  let counter = 0;
   setInterval(() => {
     const [hours, minutes, sec] = getSecondsMinutesHours();
     const turnSec = sec - 15;
     const turnMin = minutes - 15;
     const turnHours = hours < 12 ? hours - 3 : hours - 15;
-    gsap.set(secHand, {
+    
+    tl.set(secHand, {
       pixi: { rotation: turnSec * 6 },
     });
-    gsap.set(minHand, {
+    tl.set(minHand, {
       pixi: { rotation: turnMin * 6 },
     });
-    gsap.set(hourHand, {
+    tl.set(hourHand, {
       pixi: { rotation: turnHours * 30 },
     });
+    if (counter === 0) {
+      tl.pause(0.1);
+      counter++;
+    }
+    tl.play(0.1);
   }, 1000);
 }
 
